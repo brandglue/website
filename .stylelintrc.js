75009c40833924aa693b039c1c9483dc56d7c09c
Ryan Heathers <ryan@ryanheathers.com>
2019-05-02 20:37:41 -0700

    configure stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+{
+  module.exports = {
+    extends: [
+      'stylelint-config-recommended',
+      'stylelint-config-styled-components',
+      'stylelint-config-prettier',
+    ],
+    processors: ['stylelint-processor-styled-components'],
+  };
+}
