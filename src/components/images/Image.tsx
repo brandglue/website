cf5f1f7a918d6c8f5bc4b60d6d01aa25eb6fa041
Ryan Heathers <ryan@ryanheathers.com>
2020-01-13 12:32:15 -0800

    allow lazy loaded images to be loaded from cache

--- src/components/images/Image.tsx
+++ src/components/images/Image.tsx
@@ -1,37 +1,41 @@
-import React, { FC } from 'react';
-import cx from 'classnames';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 import {
   LazyLoadImage,
   LazyLoadImageProps,
 } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
 
 interface IProps extends LazyLoadImageProps {
   img: IImageGroup;
 }
 
 export const Image: FC<IProps> = ({ alt, className, img, scrollPosition }) => {
+  const [previousSrc, setPreviousSrc] = useState();
+
   const {
-    preSrc,
+    preSrc: placeholderSrc,
     src: { ...respImg },
   } = img;
-  const imageClassName = cx(className);
 
   return (
     <StyledLazyLoadImage
+      afterLoad={() => setPreviousSrc(img.src)}
       alt={alt}
-      className={imageClassName}
-      placeholderSrc={preSrc}
+      className={className}
+      effect="black-and-white"
+      placeholderSrc={placeholderSrc}
       scrollPosition={scrollPosition}
       src={respImg.src}
       srcSet={respImg.srcSet}
+      visibleByDefault={img.src === previousSrc}
     />
   );
 };
 
 Image.defaultProps = {
   alt: '',
-  className: '',
+  className: undefined,
 };
 
 const StyledLazyLoadImage = styled(LazyLoadImage)`
