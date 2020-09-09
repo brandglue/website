cd56979633a8d8636bfa9a23f3eab72743b4176d
Ryan Heathers <ryan@ryanheathers.com>
2020-09-08 22:26:59 -0700

    Improve 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,29 @@
 import React, { FC } from 'react';
 
-export const p404: FC = () => {
-  return <div>Whoops! We cannot find that page, brb.</div>;
+import { Box, Divider } from '@components/core';
+import { scale, styled } from '@styles/index';
+
+export const NotFound: FC = () => {
+  return (
+    <>
+      <Divider />
+      <Container variant="section">
+        <div>Whoops! We cannot find that page, brb.</div>
+        <ErrorCode>404</ErrorCode>
+      </Container>
+    </>
+  );
 };
 
-export default p404;
+const Container = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ErrorCode = styled.h1`
+  font-size: ${scale(2.5).fontSize};
+`;
+
+export default NotFound;
